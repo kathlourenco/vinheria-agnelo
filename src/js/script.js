058b64ba44df63df02498196e0902d3417cbc24a
kathlourenco <kathclairesl@gmail.com>
2025-04-16 11:32:55 -0300

    feat: add novo codigo js

--- src/js/script.js
+++ src/js/script.js
@@ -1,181 +1,96 @@
-
-alert("Olá! Seja bem-vindo(a) à página de cadastro de vinhos da Vinharia Agnelo!");
-
-
-
-// funcionario
-
-var funcionario = prompt("Informe o funcionário que está cadastrando o produto:")
-
-vaziofunc = (funcionario == "");
-
-if (vaziofunc) {
-
-    alert("Informe o nome de um funcionário")
-
-}
-
-var nome = prompt("Por favor, informe o nome do vinho a ser cadastrado:");
-
-vazionome = (nome == "");
-
-if (vazionome) {
-
-    alert("Por favor, informe um nome válido")
-
-}
-
-var valor = prompt("Por favor, informe o valor do vinho a ser cadastrado:");
-
-vaziovalor = (valor == "");
-
-if (vaziovalor) {
-
-    alert("Por favor, informe um valor válido")
-
-}
-
-var tipo = prompt("Agora, informe o tipo do vinho:");
-
-vaziotipo = (tipo == "");
-
-if (vaziotipo) {
-
-    alert("Por favor, informe um tipo válido")
-
-}
-
-var qtdest = prompt("Informe a quantidade disponivel no estoque:");
-
-vazioqtdest = (qtdest == "");
-
-if (vazioqtdest) {
-
-    alert("Por favor, informe uma quantidade válida")
-
-}
-
-var safra = prompt("Informe o ano da safra:");
-
-vaziosafra = (safra == "");
-
-if (vaziosafra) {
-
-    alert("Por favor, informe uma safra válida")
-
-}
-
-// sugestões de harmonização
-
-var harmonizacavinho = prompt("Informe o vinho escolhido para indicação de harmonização: Tinto, Rosé ou Branco ").toLowerCase(); 
-
-if (harmonizacaovinho === "tinto") {
-    alert("Vinho Tinto combina com queijos curados")
-} else if (harmonizacaovinho === "rosé" || harmonizacaovinho === "rose") {
-    alert("Vinho Rosé combina com saladas frescas")
-
-} else if (harmonizacaovinho === "branco") {
-    alert("Vinho Branco combina com castanhas") 
-
-} else { 
-    alert("Insira vinhos existentes na loja") 
-}
-
-// país de origem 
-var paisorigem = prompt("Informe o tipo de vinho para saber qual o seu país de origem: Tinto, Branco ou Rosé ").toLocaleLowerCase();
-
-if (paisorigem === "tinto"){
-    alert("O país de orgiem do Vinho Tinto é a Geórgia")
-} else if(paisorigem === "branco"){
-    alert("O país de origem do Vinho branco é a Geórgia")
-} else if (oaisorigem === "rosé" || paisorigem === "rose"){
-    alert("O país de origem do Vinho Rosé é a França")
-} else {
-    alert("Insira um vinho da loja")
-}
-
-// data de cadastro
-
-
-
-
-valornum = parseFloat(valor);
-
-qtdestnum = parseInt(qtdest);
-
-safranum = parseInt(safra);
-
-alert("Cadastro realizado! Veja os detalhes no console.");
-
-classafra = "";
-
-safrajovem = (safra >= 2020);
-safraamadurecida = (safra <= 2019 && safra >= 2015);
-safraantiga = (safra <= 2015);
-
-
-if (safrajovem) {
-
-    classafra = "Vinho Jovem";
-
-}
-
-else if (safraamadurecida) {
-
-    classafra = "Vinho Amadurecido";
-
-}
-
-else if (safraantiga) {
-
-    classafra = "Vinho Antigo";
-
-}
-
-else {
-
-
+let nomevinho = "";
+let tipovinho = "";
+let dataproducao = "";
+let estoque = ""; 
+
+function cadastrarVinho() {  
+   
+    nomevinho = prompt("Insira o nome de um vinho: ");
+        
+        if (nomevinho.trim() === ""){
+            alert("Insira o nome de um vinho da nossa loja!");
+            nomevinho = prompt("Insira o nome de um vinho: ");
+        }
+
+
+    tipovinho = prompt("Insira o tipo do vinho: Tinto, Branco ou Rosé: ");
+        
+    tipovinho = tipovinho.trim().toLowerCase();
+        if (tipovinho !== "tinto" && tipovinho !== "branco" && tipovinho !== "rosé" && tipovinho !== "rose"){
+            alert("Insira um tipo de vinho da nossa loja!");
+            tipovinho = prompt("Insira o tipo do vinho: Tinto, Branco ou Rosé: ");
+            tipovinho = tipovinho.trim().toLowerCase();
+        }
+
+
+    dataproducao = prompt("Insira a data de produção do vinho: ").trim();
+    dataproducao = dataproducao.trim();
+       
+        if (dataproducao === "" || parseInt(dataproducao) < 1000 || parseInt(dataproducao) > 9999){
+            alert("Insira uma data de produção válida!");
+            dataproducao = parseInt(prompt("Insira a data de produção do vinho: ")); 
+        }
+        dataproducao = parseInt(String(dataproducao).trim());
+
+
+    estoque = prompt("Insira quantos vinhos deseja levar: ");
+        if (estoque.trim() === "" || parseInt(estoque) <= 0) {
+            alert("Insira uma quantidade de vinhos válida no estoque!");
+            estoque = prompt("Insira quantos vinhos deseja levar: ");
+        }
+        if (estoque.trim() === "" || parseInt(estoque) <= 0) {
+            alert("Valor inválido! Por favor, insira uma quantidade maior que 0.");
+            estoque = prompt("Insira quantos vinhos deseja levar: ");
+    }
+        estoque = parseInt(estoque);
+        alert("A quantidade de vinhos é: " + estoque);
+} 
+cadastrarVinho();
+
+
+function detalhesVinho(nome, tipo, estoque, producao) { 
+    const mensagem = `Vinho cadastrado!
+    Nome do vinho:  ${nome}
+    Tipo do vinho:  ${tipo}
+    Estoque disponível:  ${estoque}
+    Ano de fabricação:  ${producao}`; 
+            
+            alert(mensagem);
+            alert("Veja os detalhes da sua compra no console!");
+                console.log("Detalhes da sua escolha: ");
+                console.log(`Nome: ${nome}`);
+                console.log(`Tipo:  ${tipo}`);
+                console.log(`Estoque:  ${estoque}`);
+                console.log(`Ano: " ${producao}`);
     
-}
 
-statusestoque = "";
-
-estbaixo = (qtdestnum < 5);
-estok = (qtdestnum >= 5);
-
-if (estbaixo) {
-
-    statusestoque = "ESTOQUE BAIXO";
-
-}
-
-else if (estok) {
-
-    statusestoque = "";
-
-}
-
-else {
-
-
+    if(estoque < 5){
+        console.log ("Estoque do vinho escolhido : " + estoque + "  " + "Estoque baixo!");
+    } 
+    else if(producao < 2015){
+            console.log("ano do vinho : " + producao + "  " + "  Vinho Antigo!");
+    }
+    else if(producao >= 2015 && producao <= 2019){
+            console.log("ano do vinho : " + producao + "  " + "  Vinho Amadurecido!");
+    }
+    else if(producao >= 2020 ){
+            console.log("ano do vinho : " + producao + "  " + "  Vinho jovem!");
+    }
+    else {
+        if(producao < 2015){
+            console.log("Data de produção do vinho : " + producao + "  " + "  Vinho antigo!");
+        }
+        if(producao >= 2015 && producao <= 2019){
+            console.log("Data de produção do vinho : " + producao + "   " + "  Vinho amadurecido!");
+        }
+        if(producao >= 2020 ){
+            console.log("Data de produção do vinho : " + producao + "  " + "  Vinho jovem!");
+        }
+        
+    } 
     
-}
-
-
-
-console.log(
-`
-Nome do Funcionário: ${funcionario},\n
-Nome do Produto: ${nome}, \n 
-Valor: R$ ${valornum}, \n
-Tipo: ${tipo}, \n
-Qtd. Disponível: ${qtdestnum} - ${statusestoque},\n
-Ano da Safra: ${safranum} - ${classafra} \n
-Sugestão de Harmonização: , \n 
-Data do Cadastro: , \n
-País de Origem: . 
-` 
-);
-
+} 
+ 
+    detalhesVinho(nomevinho, tipovinho, estoque, dataproducao); 
 
 
